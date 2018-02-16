56d89d42b5d8a59df537f1a89ea2b8264ba81f6f
CruzMayra <velazquez.leonora@gmail.com>
2018-02-15 23:55:52 -0600

    Creación de funciones que guarden y lean data de los commentarios

---
+++ js/data.js
@@ -0,0 +1,12 @@
+var commentsData = [
+  {
+    'text': 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem',
+    'class': 'card-body h3 text-center',
+    'style': 'color: blue; background: gray;'
+  },
+  {
+    'text': 'Li Europan lingues es membres del sam familie. Lor separat existentie es',
+    'class': 'card-body h5 text-left',
+    'style': 'color: rebeccapurple; background: whitesmoke;'
+  }
+];
